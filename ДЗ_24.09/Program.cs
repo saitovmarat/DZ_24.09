3f9ceee39ae22e04b7ac5cdcfe105115817c5c11
saitovmarat <maratsajtov@gmail.com>
2022-09-26 18:36:24 +0300

    Добавьте файлы проекта.

---
+++ ДЗ_24.09/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ДЗ_24._09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Task 1");
+            //for (int a = 0; ;a++)
+            //{
+            //     if 
+            // }
+
+            //Console.WriteLine("Task 2");
+            //Console.Write("Напишите число: ");
+            //int num = int.Parse(Console.ReadLine());           
+            //Console.WriteLine($"Таблица умножения с числом {num}" + "\n" + 
+            // $"{num}*1 = {num*1}" + "\n" +
+            //  $"{num}*2 = {num * 2}" + "\n" +
+            //  $"{num}*3 = {num * 3}" + "\n" +
+            //   $"{num}*4 = {num * 4}" + "\n" +
+            //  $"{num}*5 = {num * 5}" + "\n" +
+            //    $"{num}*6 = {num * 6}" + "\n" +
+            //    $"{num}*7 = {num * 7}" + "\n" +
+            //   $"{num}*8 = {num * 8}" + "\n" +
+            //   $"{num}*9 = {num * 9}" + "\n");
+
+            //Console.WriteLine("Task 3");
+            //    Console.Write("Напишите число: ");
+            //   int num = int.Parse(Console.ReadLine());
+            //   if ((num % 5 == 0) & (num % 3 == 0))
+            //       Console.WriteLine("БэнгБум");
+            //   else if (num % 3 == 0)
+            //      Console.WriteLine("Бэнг");
+            //  else if (num % 5 == 0)
+            //      Console.WriteLine("Бум");
+            //   else
+            //    Console.WriteLine("Мимо");
+
+            Console.WriteLine("Task 4");
+
+        }
+    }
+}
